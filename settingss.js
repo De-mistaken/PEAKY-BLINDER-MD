b0a2cde2b4e13871a3b1cd86c057af6988b2d36e
De-mistaken <mutethiateddy606@gmail.com>
2025-11-02 22:53:49 +0300

    Update SESSION_ID in settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~QNQxHZ5C#DbLIOlvXjX0XmVqL6GhHCuvM4nhJEkhEqgiNfe3_eW4",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~9BJGwYJJ#8LUAyUhgWMBr6vaslQXp8hKtyEnskWEVLBF5cwjwQok",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
