f4fd6a59171f7d17dd6d7f10c397d09b7d25393b
De-mistaken <mutethiateddy606@gmail.com>
2025-11-17 15:42:43 +0300

    Update OWNER_NUMBER in settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: "blinder~5BASwa7C#pH-_lbOKnL0F9Uk4BTyvXymfZX5JTWpYrkAAtdqfVGE",  // add your Session Id make sure it starts with blinder~
-  OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
+  OWNER_NUMBER: "254762325918", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
